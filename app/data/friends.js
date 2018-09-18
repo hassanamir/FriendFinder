9e6b6c3855146358cd6b1273948a0b91376ce175
AMIR  HASSAN <amirhassan@AMIRs-MacBook-Air.local>
2018-09-17 22:31:57 -0400

    first time deploy on heroku

---
+++ app/data/friends.js
@@ -0,0 +1,179 @@
+var friends =[ 
+    {
+    "name":"drake",
+    "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.billboard.com%2Ffiles%2Fmedia%2FDrake-Summer-16-MSG-aug-8-2016-billboard-1548.jpg&imgrefurl=https%3A%2F%2Fwww.billboard.com%2Farticles%2Fcolumns%2Fhip-hop%2F8469963%2Fdrake-scorpion-videos-ranked-list&docid=VdbCyYR821PJMM&tbnid=99uTasQYm1Q6_M%3A&vet=10ahUKEwiItuf1rMDdAhWrmuAKHQqgA18QMwiaAigJMAk..i&w=1548&h=1024&bih=821&biw=1440&q=drake&ved=0ahUKEwiItuf1rMDdAhWrmuAKHQqgA18QMwiaAigJMAk&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+    },
+
+    {
+    "name":"future",
+    "photo":"https://www.google.com/imgres?imgurl=http%3A%2F%2Fmajoumo.com%2Fimages%2Ffuture%2Ffuture16.jpg&imgrefurl=http%3A%2F%2Fmajoumo.com%2Fimages%2Ffuture%2Ffuture16.html&docid=gQKUk5nzSLvF2M&tbnid=OJ-e8-dI6GuoTM%3A&vet=10ahUKEwiE0tqBpsLdAhURMt8KHamHDO8QMwjrASgZMBk..i&w=960&h=640&bih=821&biw=1440&q=future&ved=0ahUKEwiE0tqBpsLdAhURMt8KHamHDO8QMwjrASgZMBk&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        1,
+        5,
+        4,
+        1
+    ]
+    },
+
+    {
+    "name":"Justin beiber",
+    "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fbloximages.chicago2.vip.townnews.com%2Fjournalstar.com%2Fcontent%2Ftncms%2Fassets%2Fv3%2Feditorial%2F0%2F3b%2F03bd8b72-08e5-5358-a663-769d55d80c18%2F5b979b33639f1.image.jpg&imgrefurl=https%3A%2F%2Fjournalstar.com%2Fentertainment%2Fmusic%2Fjustin-bieber-bonds-with-stephen-baldwin%2Farticle_289517f0-457d-58dc-a672-40a1221fee56.html&docid=ddii_cj0jGGSsM&tbnid=ReJ_AztKU4RTLM%3A&vet=10ahUKEwiYyvbypcLdAhVRmVkKHZ95BRIQMwihAyhRMFE..i&w=1000&h=650&itg=1&bih=821&biw=1440&q=justin%20bieber&ved=0ahUKEwiYyvbypcLdAhVRmVkKHZ95BRIQMwihAyhRMFE&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        4,
+        4,
+        1
+    ]
+    },
+
+
+    {
+    "name":"Eminem",
+    "photo":"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwixhb3jpcLdAhWvTd8KHW57DYEQjRx6BAgBEAU&url=http%3A%2F%2Fhiphopdx.com%2Fnews%2Fid.48385%2Ftitle.hip-hop-album-sales-eminems-kamikaze-soars-to-no-1-on-billboard-200-with-epic-debut&psig=AOvVaw1eJ982es_oF-8dCdrCr9oL&ust=1537282510740495",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        3,
+        2,
+        5,
+        4,
+        1
+    ]
+    },
+
+    {
+    "name":"Post Malone",
+    "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fi0.wp.com%2Fwww.usmagazine.com%2Fwp-content%2Fuploads%2F2018%2F07%2Fpost-malone-meatballs-fruit.jpg%3Fcrop%3D164px%252C0px%252C1690px%252C955px%26resize%3D1200%252C675%26ssl%3D1&imgrefurl=https%3A%2F%2Fwww.usmagazine.com%2Fcelebrity-news%2Fnews%2Frapper-post-malone-thinks-meatballs-are-fruit%2F&docid=xzyRTXBg2778rM&tbnid=TBJUfL2QQXjlhM%3A&vet=10ahUKEwjoj4XHpcLdAhVmuVkKHWDHDkQQMwjXAihUMFQ..i&w=1200&h=675&bih=821&biw=1440&q=post%20malone&ved=0ahUKEwjoj4XHpcLdAhVmuVkKHWDHDkQQMwjXAihUMFQ&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        4,
+        1,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+    },
+
+    {
+    "name":"lil yachty",
+    "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fd3vhc53cl8e8km.cloudfront.net%2Fhello-staging%2Fwp-content%2Fuploads%2F2018%2F01%2F26224517%2Fc3z9alfASMCoSiLctagxTKh9tas35tfBcuPmlCIP-972x597.jpeg&imgrefurl=https%3A%2F%2Fwww.insomniac.com%2Fmusic%2Fartists%2Flil-yachty%2F&docid=BQ--naHNfthlhM&tbnid=VyYrqhk5rAPItM%3A&vet=10ahUKEwiuj-O1pcLdAhUomeAKHS1BAVQQMwizAigUMBQ..i&w=972&h=597&bih=821&biw=1440&q=lil%20yachty&ved=0ahUKEwiuj-O1pcLdAhUomeAKHS1BAVQQMwizAigUMBQ&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        1,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+    },
+
+    {
+    "name":"Selena Gomez",
+    "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fassets.teenvogue.com%2Fphotos%2F571029a8a290d7501d1acb43%2Fmaster%2Fpass%2Fselena-gomez-my-world-tout.jpg&imgrefurl=https%3A%2F%2Fwww.teenvogue.com%2Fgallery%2Fthings-you-didnt-know-about-selena-gomez&docid=2x4VbubiXdF0QM&tbnid=dWjOW6fynuSDJM%3A&vet=10ahUKEwiiyvShpcLdAhVPGt8KHc2IAAIQMwisAighMCE..i&w=2993&h=2993&bih=821&biw=1440&q=selena%20gomez&ved=0ahUKEwiiyvShpcLdAhVPGt8KHc2IAAIQMwisAighMCE&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        3,
+        5,
+        4,
+        1
+    ]
+    },
+ 
+    {
+    "name":"Lil baby",
+    "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fd-2KRSezHb0%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dd-2KRSezHb0&docid=HjFZeSsRwUqsEM&tbnid=0faUqtUIKoe70M%3A&vet=10ahUKEwjF96uIpcLdAhUxWN8KHZlYBHYQMwiBASgXMBc..i&w=1280&h=720&bih=821&biw=1440&q=lil%20baby&ved=0ahUKEwjF96uIpcLdAhUxWN8KHZlYBHYQMwiBASgXMBc&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        4,
+        5,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+    },
+ 
+    {
+    "name":"kevin hart",
+    "photo":"https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww2.philly.com%2Fresizer%2FlaP8SN5-yT0D4y5Ql3TZaBhv_9Q%3D%2F1400x0%2Fcenter%2Fmiddle%2Farc-anglerfish-arc2-prod-pmn.s3.amazonaws.com%2Fpublic%2FDBTWGCGWV5C7ZJBACNCBPOCUWE.jpg&imgrefurl=http%3A%2F%2Fwww2.philly.com%2Fphilly%2Fentertainment%2Fcelebrities%2Fkevin-hart-donald-trump-vmas-20180821.html&docid=nmC5t1lsJGtb0M&tbnid=2LJ4ymB2uD95MM%3A&vet=10ahUKEwj8toX2pMLdAhVRm-AKHVm0ANIQMwg6KAowCg..i&w=1400&h=932&bih=821&biw=1440&q=kevin%20hart&ved=0ahUKEwj8toX2pMLdAhVRm-AKHVm0ANIQMwg6KAowCg&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        4
+    ]
+    },
+ 
+    {
+    "name":"offset",
+    "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fpmcvariety.files.wordpress.com%2F2018%2F07%2Foffset-migos-2018_edited.jpg%3Fw%3D1000&imgrefurl=https%3A%2F%2Fvariety.com%2F2018%2Fmusic%2Fnews%2Fmigos-offset-arrested-cardi-b-1202880387%2F&docid=wm3dOhvkEFMV4M&tbnid=uabSwG2uZyZg7M%3A&vet=10ahUKEwjP75HcpMLdAhVoRN8KHSFxAvIQMwhdKCEwIQ..i&w=1000&h=563&bih=821&biw=1440&q=offset&ved=0ahUKEwjP75HcpMLdAhVoRN8KHSFxAvIQMwhdKCEwIQ&iact=mrc&uact=8",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        3,
+        2,
+        5,
+        4,
+        1
+    ]
+    },
+];
+module.exports = friends;
+
+
+
+
+
+
